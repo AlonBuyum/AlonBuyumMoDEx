e89e14911e0a981bb048bae414271a068d2fa2e0
אלון בויום <Balon@bazan.co.il>
2025-02-11 20:20:08 +0200

    Added Categories Controller and Shopping.db

--- AlonBuyumMoDEx.Server/Controllers/CategoriesController.cs
+++ AlonBuyumMoDEx.Server/Controllers/CategoriesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlonBuyumMoDEx.Server.DAL;
+using AlonBuyumMoDEx.Server.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,38 +10,42 @@ namespace AlonBuyumMoDEx.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoriesController : ControllerBase 
+    public class CategoriesController : ControllerBase
     {
-        // GET: api/<CategoriesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ShoppingContext _shoppingContext;
+
+        public CategoriesController(ShoppingContext shoppingContext)
         {
-            return new string[] { "value1", "value2" };
+            _shoppingContext = shoppingContext;
+        }
+        // GET: api/GetAllCategories
+        [HttpGet, Route("GetAllCategories")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await _shoppingContext.Categories.ToListAsync();
+            return Ok(categories);
         }
 
-        // GET api/<CategoriesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET: api/GetCategoryById/5
+        [HttpGet, Route("GetCategoryById/{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
         {
-            return "value";
+            var category = await _shoppingContext.Categories.FindAsync(id);
+            if (category == null) return NotFound("Category not found");
+            return Ok(category);
         }
 
-        // POST api/<CategoriesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // POST: api/AddCategory
+        [HttpPost, Route("AddCategory")]
+        public async  Task<IActionResult> AddCategory([FromBody]Category category)
         {
-        }
+            if (category == null) return BadRequest("Category is null");
 
-        // PUT api/<CategoriesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+            var dbCategory =  _shoppingContext.Categories.Add(category);
+            var saved = await _shoppingContext.SaveChangesAsync();
 
-        // DELETE api/<CategoriesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (saved < 0) return StatusCode(500, "Failed to save Category");
+            return Ok(dbCategory.Entity);
         }
     }
 }
